3c85da60c12f21dcf546232fe81d204f109ab360
CzechCoder <92126675+CzechCoder@users.noreply.github.com>
2022-04-12 17:33:05 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    cors({
+      origin: "*",
+    })
+    );
   await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
